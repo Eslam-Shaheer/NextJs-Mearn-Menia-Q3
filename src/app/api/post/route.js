1e9b9a73766c4e53ec46dfd9c07630b3ccfd6619
Eslam-Shaheer <eslamshaheer@gmail.com>
2024-08-21 12:17:47 +0300

    Remove database connection from route files

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -3,20 +3,17 @@ import { cookies } from "next/headers";
 
 export async function GET(request) {
   //   console.log(request.nextUrl.searchParams);
-  const client = await clientPromise;
-  const db = client.db();
-  const news = await db.collection("news").find({}).toArray();
-
-  return new Response(JSON.stringify(news), { status: 200 });
+  // const client = await clientPromise;
+  // const db = client.db();
+  // const news = await db.collection("news").find({}).toArray();
+  // return new Response(JSON.stringify(news), { status: 200 });
 }
 
 export async function POST(request) {
-  const data = await request.json();
-
-  const client = await clientPromise;
-  const db = client.db();
-  await db.collection("posts").insertOne(data);
-
-  return new Response("success", { status: 201 });
+  // const data = await request.json();
+  // const client = await clientPromise;
+  // const db = client.db();
+  // await db.collection("posts").insertOne(data);
+  // return new Response("success", { status: 201 });
 }
 export async function PUT(request) {}
